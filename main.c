22e88f55f94b6b8b5f35c738d9a5436fd7722f5e
canoswright-blip <canos.wright@gmail.com>
2025-12-03 21:49:35 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,49 @@
+#pragma config FNOSC = LPFRC //choose the internal 500 kHz oscillator
+#pragma config OSCIOFNC = OFF   // Turn off clock output on pin 8
+#pragma config SOSCSRC = DIG    // Turn off secondary oscillator on pins 9&10
+#include "xc.h"
+#include "setup.h"
+#include "global_variables.h"
+
+int main(void) {
+    // Post-scale oscillator
+    _RCDIV = 0b000;             // Divide-by-1 post-scaler
+
+    // Configure pins, ADC, PWM, Timer1
+    config_pins();
+    config_ad(); 
+    config_pwm();
+    config_tmr1();
+    
+    initial_OCregisters(); // Initialize OC1RS and OC1R
+    wheel_dir(); // Set wheel direction
+    
+    //initialize state here
+    state current_state = COLLECT;
+    
+    while (1) 
+    {
+        switch(current_state) 
+        {
+            case LINEFOLLOW: // If current_state == LINEFOLLOW
+                _LATB2 = 1;
+                line_follow();
+                break;
+            case COLLECT: // If current_state == COLLECT
+                ball_collect();
+                current_state = LINEFOLLOW;
+                break;
+//            case RETURN: // If current_state == RETURN
+//                ball_return();
+//                break;
+//            case CANYON: // If current_state == CANYON
+//                nav_canyon();
+//                break;
+//            case TRANSMIT: // If current_state == TRANSMIT
+//                transmit_sig();
+//                break;
+        }
+    }
+    
+    return 0;
+}
