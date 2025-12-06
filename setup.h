2440adaa617c9d75b625036e401007c8972b1934
canoswright-blip <canos.wright@gmail.com>
2025-12-05 21:37:26 -0700

    Update setup.h

--- setup.h
+++ setup.h
@@ -9,6 +9,7 @@
 void line_follow(void);
 void ball_collect(void);
 void ball_return(void);
+//int nav_canyon(int previous_state);
 void nav_canyon(void);
 void transmit_sig(void);
 
@@ -19,6 +20,7 @@ void config_pwm(void);
 void config_tmr1(void);
 void initial_OCregisters(void);
 void wheel_dir(void);
+void config_OCinterrupt(void);
 
 // some ball_collect function prototypes
 void turn_right_collect(void);
@@ -26,6 +28,16 @@ void turn_left_collect(void);
 void pause(void);
 void collect(void);
 void backup(void);
+
+// ball_return function prototypes
+void delay(void);
+void turn_right_return(void);
+void turn_left_return(void);
+
+// nav_canyon function prototypes
+void turn_right_canyon(void);
+void turn_left_canyon(void);
+
 
 #endif	/* XC_HEADER_TEMPLATE_H */
 
